a1505e8e9a91ccfb65f78a10585b3f7f6d4e7123
심세린 / 201311219 <sim3272@naver.com>
2016-10-14 18:12:38 +0900

    Add files via upload

---
+++ p_rest_3.py
@@ -0,0 +1,23 @@
+﻿import os
+
+KEY = "476641415973736131303175436b506f"
+TYPE = 'json'
+SERVICE = 'SearchSTNBySubwayLineService'
+START_INDEX='1'
+END_INDEX='10'
+LINE_NUM='1'
+url='http://openapi.seoul.go.kr:8088/'
+url+=KEY
+url+='/'
+url+=TYPE
+url+='/'
+url+=SERVICE
+url+='/'
+url+=START_INDEX
+url+='/'
+url+=END_INDEX
+url+='/'
+url+=LINE_NUM
+import requests
+data =requests.get(url)
+print data.text
